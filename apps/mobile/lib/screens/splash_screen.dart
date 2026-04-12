817dbfe0e7091264b7cf136c2464a5731e83f99d
EPI Expert <epi@supervisor.dev>
2026-04-13 07:02:12 +0800

    fix: add missing supabase_flutter import in splash_screen

--- apps/mobile/lib/screens/splash_screen.dart
+++ apps/mobile/lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:epi_shared/epi_shared.dart';
 import 'package:epi_core/epi_core.dart';
 
